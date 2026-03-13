9c1f743659a9fa51da5e5fde6f1e4f8b1108db0c
Camilo Delgado <camilodelgado@Camilos-Createme-MacBook-Air.local>
2026-03-13 11:13:41 -0500

    Añadiendo cambios a la logica de traduccion. implementando diccionario unico de braile y una funcion unica traductora

--- traductor.py
+++ traductor.py
@@ -1,6 +1,3 @@
-
-
-
 from dicts import (alfabeto_braille)
 from archivos import guardarArchivo
 
@@ -10,6 +7,12 @@ def pedirTexto(palabra=""):
 
 #Traduccion letra a letra   
 def traducir_caracter_a_braile(text: str):
+    """
+    Funcion que traduce numeros enteros a braile. compara numero a numero
+     input: numeros String
+     output: numeros String: a Braile
+    """
+    if not text: return ""
     resultado = ""
     for letra in text.lower(): 
         if letra in alfabeto_braille:
@@ -17,47 +20,18 @@ def traducir_caracter_a_braile(text: str):
         else:
             resultado += letra
     print("\n")
-    print("Palabra traducida a Braille: ", text)
+    print("Palabra a traducir al Braille: ", text)
     print("Palabra en braile: ", resultado)
-    return resultado
-
-
-
-
-
-def traducir_numeros_a_braille(texto_numerico):
-    """
-    Funcion que traduce numeros enteros a braile. compara numero a numero
-     input: numeros String
-     output: numeros String: a Braile
-    """
-
-    if not texto_numerico:
-        return ""
-    prefijo_numero = '⠼'
-    resultado = prefijo_numero
-    for caracter in str(texto_numerico):
-        if caracter in alfabeto_braille:
-            resultado += alfabeto_braille[caracter]
-    print("\n")
-    print("Numero traducida a Braille: ", texto_numerico)
-    print("Numero a braile: ", resultado)
     return resultado
 
 def main():
     entrada = pedirTexto("Ingrese el texto o número a traducir: ")
     
     if not entrada:
-        print("No ingresaste nada.")
+        print("No ingresaste ningun texto.")
         return
-
-    # Identificamos si es número o texto
-    if entrada.isdigit():
-        print("Detectado: Entrada numérica.")
-        resultado_final = traducir_numeros_a_braille(entrada)
-    else:
-        print("Detectado: Entrada de texto.")
-        resultado_final = traducir_caracter_a_braile(entrada)
+    
+    resultado_final = traducir_caracter_a_braile(entrada)
 
     # Guardamos el resultado obtenido en el TXT
     guardarArchivo("español_abraile.txt",resultado_final)

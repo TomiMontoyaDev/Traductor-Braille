52f47e70ba942f7286bb6ff0b23937c9d7258711
Camilo Delgado <camilodelgado@Camilos-Createme-MacBook-Air.local>
2026-03-14 12:45:12 -0500

    Añadiendo una mini documentacion a la funcion guaradarArchivo

--- archivos.py
+++ archivos.py
@@ -1,4 +1,15 @@
+
+
 def guardarArchivo(path:str, contenido:str ):
+    """
+    funcion para guardar un archivo txt
+    
+    parametro path:str direccion donde se quiere guardar el archivo, por defecto
+    la carpeta raiz del proyecto 
+    
+    param contenido:str El texto el ual es el que se desea añadir al archvo. puede ser usado para español o braile
+    
+    """
     with open(path, "w", encoding="utf-8") as archivo:
         archivo.write(contenido)
     print(f"El contenido se a guardado satisfactoriamente en la direccion {path}")

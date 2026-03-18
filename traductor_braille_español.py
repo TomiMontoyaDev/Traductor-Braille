5f8bee79f76364699293832fa6f087ccb3ce86d2
nikimv <nicole.marin@utp.edu.co>
2026-03-17 21:26:26 -0500

    ultimo arreglito, ya tenemos numeros

--- traductor_braille_español.py
+++ traductor_braille_español.py
@@ -7,8 +7,21 @@ braille_dict = {
     '⠟': 'q', '⠗': 'r', '⠎': 's', '⠞': 't',
     '⠥': 'u', '⠧': 'v', '⠺': 'w', '⠭': 'x',
     '⠽': 'y', '⠵': 'z', ' ': ' ',  # Espacio en Braille
-}
 
+    # Acentuadas
+    'á': '⠷', 'é': '⠿', 'í': '⠾', 'ó': '⠓', 'ú': '⠜', 'ü': '⠳',
+    # Espacio
+    ' ': '⠀',
+    # Puntuación básica
+    '.': '⠲', ',': '⠂', '?': '⠦', '!': '⠖',
+
+}
+PREFIJO_NUMERO = '⠼'
+numeros_braille = {
+    '1': '⠁', '2': '⠃', '3': '⠉', '4': '⠙', '5': '⠑',
+    '6': '⠋', '7': '⠛', '8': '⠓', '9': '⠊', '0': '⠚',
+}
+braille_numeros_invertidos = {v: k for k, v in numeros_braille.items()}  # Para traducir de Braille a números
 def traducir_braille(braille_texto):
     """
     Traduce una cadena de Braille (símbolos Braille) a español.
@@ -16,8 +29,12 @@ def traducir_braille(braille_texto):
     """
     letras = braille_texto.strip()
     resultado = ''
-    for letra in letras:
-        resultado += braille_dict.get(letra, '?')
+    for i,letra in enumerate(letras):
+        if letra == '⠼' or letras [i-1] == '⠼':  # Prefijo de número, se ignora en la traducción
+            if letra in braille_numeros_invertidos:
+                resultado += braille_numeros_invertidos[letra]
+        else:
+             resultado += braille_dict.get(letra, ' ')
     return resultado
 
 if __name__ == "__main__":
